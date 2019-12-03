02bad23507a845f7cc47836148e95bd941ef8e92
Haibin Lin <linhaibin.eric@gmail.com>
2019-12-02 22:31:30 -0800

    Add README

---
+++ demo/README.md
@@ -0,0 +1,6 @@
+### Run Object Detection and Pose Estimation on Your Laptop
+
+- `conda env create -f gluoncv.yml`
+- `conda activate gluoncv`
+- `python object_detection_demo.py`
+- `python pose_estimation_demo.py`
--- demo/pose_estimation_demo.py
+++ demo/pose_estimation_demo.py
@@ -1,12 +1,6 @@
 # """3. Estimate pose from your webcam
 # ====================================
-# 
 # This article will demonstrate how to estimate people's pose from your webcam video stream.
-# 
-# 
-# First, import the necessary modules.
-# 
-# .. code-block:: python
 
 from __future__ import division
 import argparse, time, logging, os, math, tqdm, cv2
@@ -28,29 +22,20 @@ from gluoncv.utils.viz import cv_plot_image, cv_plot_keypoints
 
 # Loading the model and webcam
 # ----------------------------
-# 
 # In this tutorial we feed frames from the webcam into a detector, 
 # then we estimate the pose for each detected people in the frame.
-# 
 # For the detector we use ``ssd_512_mobilenet1.0_coco`` as it is fast and accurate enough.
-# 
-# 
-# .. code-block:: python
 
 ctx = mx.cpu()
 detector_name = "ssd_512_mobilenet1.0_coco"
 detector = get_model(detector_name, pretrained=True, ctx=ctx)
 
-
 # The pre-trained model tries to detect all 80 classes of objects in an image,
 # however in pose estimation we are only interested in one object class: person.
-# 
 # To speed up the detector, we can reset the prediction head to only include the classes we need.
-# 
-# .. code-block:: python
 
 detector.reset_class(classes=['person'], reuse_weights={'person':'person'})
-
+detector.hybridize()
 
 # Next for the estimator, we choose ``simple_pose_resnet18_v1b`` for it is light-weighted.
 # 
@@ -58,40 +43,24 @@ detector.reset_class(classes=['person'], reuse_weights={'person':'person'})
 # We also provide an optional ``simple_pose_resnet18_v1b`` model trained with input size 128x96.
 # The latter one is going to be faster, which means a smoother webcam demo.
 # Remember that we can load an optional pre-trained model by passing its shasum to ``pretrained``.
-# 
-# .. code-block:: python
 
 estimator = get_model('simple_pose_resnet18_v1b', pretrained='ccd24037', ctx=ctx)
-
+estimator.hybridize()
 
 # With OpenCV, we can easily retrieve frames from the webcam.
-# 
-# .. code-block:: python
-
 cap = cv2.VideoCapture(0)
 time.sleep(1)  ### letting the camera autofocus
 
 
-# .. note::
-# 
-#     In the code we run the demo on CPU, if your machine has a GPU then you may try heavier and more accurate
-#     pre-trained detectors and estimators.
-#     
-#     For a list of models to choose from, please refer to our Model Zoo pages for detection and pose estimation.
-# 
 # Estimation loop 
 # --------------
-# 
 # For each frame, we perform the following steps:
-# 
 # - loading the webcam frame
 # - pre-process the image
 # - detect people in the image
 # - post-process the detected people
 # - estimate the pose for each person
 # - plot the result
-# 
-# .. code-block:: python
 
 axes = None
 num_frames = 100
@@ -100,7 +69,7 @@ for i in range(num_frames):
     ret, frame = cap.read()
     frame = mx.nd.array(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).astype('uint8')
 
-    x, frame = gcv.data.transforms.presets.yolo.transform_test(frame, short=512, max_size=350)
+    x, frame = gcv.data.transforms.presets.ssd.transform_test(frame, short=512, max_size=350)
     x = x.as_in_context(ctx)
     class_IDs, scores, bounding_boxs = detector(x)
 
@@ -115,36 +84,5 @@ for i in range(num_frames):
     cv_plot_image(img)
     cv2.waitKey(1)
 
-
 # We release the webcam before exiting:
-# 
-# 
-# .. code-block:: python
-
 cap.release()
-
-# Results
-# -------
-# 
-# Download the script to run the demo
-# 
-# :download:`Download cam_demo.py<../../../scripts/pose/simple_pose/cam_demo.py>`
-# 
-# Run the script 
-# 
-# .. code-block:: bash
-# 
-#     python cam_demo.py --num-frames 100
-# 
-# 
-# If all goes well you should be able to see your pose detected!
-# 
-# .. image:: https://i.giphy.com/media/1kTFyZCOCA4yilyOHk/giphy.gif
-# 
-# 
-# The input size significantly affect the inference speed.
-# Below is the webcam demo with input 256x192, compare the frames per second!
-# 
-# .. image:: https://i.giphy.com/media/8rFv0lvBgGf62CIcM7/giphy.gif
-# 
-# """
